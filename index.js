cced8d6d5514b57025aaebfe6ac78468ad4849ae
Jordan Brantner <thejordanbrantner@gmail.com>
2023-10-06 16:25:44 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+
+function findMatching(array, name){
+    const findNames = array.filter(function(element){return element.toLowerCase() === name.toLowerCase()})
+    console.log(findNames);
+    return findNames;
+}
+
+
+
+function fuzzyMatch(array, name){
+    const findLetters = array.filter(function(element){return element[0] === name[0]})
+    console.log(findLetters);
+    return findLetters;
+}
+
+function matchName(driver, string){
+    const findMatches = driver.filter(function(element){return element.name === string})
+    console.log(findMatches);
+    return findMatches;
+}
